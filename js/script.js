73ec028d7768b9e9d4b01920d1599c98512d9f60
Hawker1228 <99539292+Hawker1228@users.noreply.github.com>
2023-09-18 15:47:53 +0300

    JS\CSS\HTML New_1.01
    Обновленная версия, наработки за 18.09.23

--- js/script.js
+++ js/script.js
@@ -44,38 +44,54 @@
 // }
 // string_1();
 
-// dadagagdfgjpspgls12412!@#$%^&*()
+// dadagagdfgjpspgls12412!@#$%^&*() все символы по 10%
 function string_2() {
- let text_vivod = document.getElementById('text1');
+    let text_vivod = document.getElementById('text1');
 
-    const s = "*!!??:";
+    const s = "dadagagdfgjpspgls12412!@#$%^&*()";
     const num_char = /[^a-zа-яA-ZА-Я0-9]+/g;
     const char = /[^a-zа-яA-ZА-Я]+/g;
     const num = /[^0-9]+/g;
-    const musor = /[^!-\/:-@[-`{-~]/gm;
+    const musor = /[^!-\/№:-@[-`{-~]/gm;
     
-    // console.log('Ваш набор данных - \n' + s);
-    let musor_sort = s.replace(musor, "")
-    console.log('Мусор - ' + musor_sort);
-
-let len = musor_sort.length
-let arr1 = [];
+    let musor_sort = s.replace(musor, "") //Делаю выборку по знакам
+    console.log('Мусор - ' + musor_sort); //отображаю их все в консоль
+    let len = musor_sort.length //Получаем количество всех символов
+    let arr1 = [];
 
     for(let i=0; i < len; i++){
-        arr1.push(musor_sort[i]);
-        // console.log(arr1[i] + " " +  arr1[i] / len*100);
+        arr1.push(musor_sort[i]); //Заполняю  массив arr1
     }
+    console.log(arr1);
+  
+    var sum_sim = []; //Создаю промежуточный массив для перебора  
+    console.log(sum_sim);
 
-var sum_sim = {};
-    arr1.forEach(function(a){
-        if (sum_sim[a] != undefined)
-            ++sum_sim[a];
-        else
-        sum_sim[a] = 1;
-    });
+        arr1.forEach(function(a){ //Проходим по массиву arr1
+            if (sum_sim[a] != undefined)
+              ++sum_sim[a];  // Заполняем уникальными символами
+            else
+            sum_sim[a] = 1;  // Считаем каждый +1
+            });
+         console.log(sum_sim);
+
+    var sum_key = 0;
     for (var key in sum_sim) {
-    document.write('Символ ' + key + ' == ' + (sum_sim[key] / len*100).toFixed(1) + ' %'+ "\r");    
-    }
- 
+            var doc_txt = ('Символ ' + key + ' == '); 
+            var doc_p =  text_vivod.appendChild(document.createElement("p"));
+            doc_p.textContent = doc_txt;
+            var doc_a = doc_p.appendChild(document.createElement("a")).textContent=((sum_sim[key] / len*100).toFixed(1) + '%');
+            var h_txt = key.length;
+            sum_key += h_txt;
+        }
+        console.log(sum_key);
+        
+        text_vivod.style.height+=(sum_key*35)+"px";
 }
-string_2();   
+
+let dollarsCount = 50 * 1.25;
+const dollar =  document.getElementById("var_txt");
+let dollar_p = dollar.appendChild(document.createElement("p")).innerText='Huy';
+
+console.log(dollarsCount);
+
